9cc38868f4654b249566f99e677d707969222549
walter1709 <32747258+walter1709@users.noreply.github.com>
2018-04-04 21:23:43 -0400

    deleting duplicates

--- databaseinfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//server connection access
-
-	$server="localhost";
-	$login="id4888052_wsj";
-	$password="quickatt";
-	$dbname="id4888052_quickatt";
-
-?>
--- quickatt_attendance.php
+++
@@ -1,4 +0,0 @@
-<?php
-//change attendance status
-
-?>
