39dd6fc3a33118b173edc3185a3f781e092488d8
walter1709 <32747258+walter1709@users.noreply.github.com>
2018-04-11 11:19:26 -0400

    db file changed

--- includes/databaseinfo.php
+++ includes/databaseinfo.php
@@ -6,4 +6,7 @@
 	$password="quickatt";
 	$dbname="id4888052_quickatt";
 
+	$con = @mysqli_connect ("localhost", "id4888052_wsj", "quickatt", "id4888052_quickatt") OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+
+
 ?>
