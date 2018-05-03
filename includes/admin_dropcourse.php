77a4a5968ad4bce46a7aa838abbc9bcdcc24166e
wsjquickatt <37782867+wsjquickatt@users.noreply.github.com>
2018-05-02 21:16:11 -0400

    drop course from system (Admin)

---
+++ includes/admin_dropcourse.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+
+	if((!isset($_SESSION['login'])) || ($_SESSION['login'] == false))
+	{
+		header("location: index.html");
+	}
+//drop course;  admin role
+
+	include ("databaseinfo.php");
+	
+	$roleid = $_SESSION['roleid'];
+	$userid = $_SESSION['userid'];
+	$course_id = $_POST['courseid'];
+	$section_id = $_POST['sectionid'];
+	$cid = $_POST['cid'];
+
+	$querycheck = "SELECT q.* FROM id4888052_quickatt.classes q WHERE course_id = '$course_id' AND section_id = '$section_id';";
+	$resultcheck = mysqli_query($con, $querycheck);
+	$rowcount = mysqli_num_rows($resultcheck);
+
+	$querydelete= "DELETE FROM id4888052_quickatt.users WHERE user_id = '$user_id';";
+	if($resultcheck)
+	{
+		if($rowcount == 0)
+		{
+			exit("The course does not yet exist.");
+		}
+		elseif($rowcount > 0)
+		{
+			$resultdelete = mysqli_query($con, $querydelete);
+			if($resultdelete)
+			{
+				if(mysqli_affected_rows($con))
+				{
+				echo "$course_id - $section_id was successfully deleted from Classes! <br>";
+				$query="SELECT q.* from id4888052_quickatt.classes q;";
+				$result=mysqli_query($con, $query);
+			
+				echo "<TABLE border=1>\n";
+				echo "<TR><TD>CID<TD>Course id<TD>Section ID\n";
+				while($row=mysqli_fetch_array($result))
+				{
+					$course=$row['course_id'];
+					$section=$row['section_id'];
+					$cid=['cid'];
+
+					echo "<TR><TD>$cid<TD>$course<TD>$section\n";
+				}	
+				echo "</TABLE>\n"
+				}		
+			}
+			else
+			{
+				exit("An error has occurred.");
+			}
+		}
+	}
+
+?>
