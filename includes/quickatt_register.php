07ab606e9d08c87e8a55f57d9aebf5571f990d89
wsjquickatt <37782867+wsjquickatt@users.noreply.github.com>
2018-05-06 02:06:16 -0400

    updated error messages

--- includes/quickatt_register.php
+++ includes/quickatt_register.php
@@ -30,24 +30,39 @@
 		$role_id = 2; //faculty
 
 	$query="INSERT INTO id4888052_quickatt.users (user_id, role_id, firstname, lastname, email, pwd) VALUES ('$user_id', '$role_id', '$fname', '$lname', '$email', '$password');";
-	if(ctype_digit ($user_id)) // check if numberic value for userID
+	$querycheck="SELECT q.* FROM id4888052_quickatt.users q WHERE user_id = '$user_id';";
+	$resultcheck= mysqli_query($con, $querycheck);
+	$rowcount = mysqli_num_rows($resultcheck);
+	
+	if($rowcount == 0)
 	{
-		$result=mysqli_query($con, $query);	
-		if($result)
-		{
-			echo "You have been successfully registered!";
-			echo "<a href= 'index.html'>Return to login</a>";
-		}
-		else
-			echo "There has been an error.";
+	    if(ctype_digit ($user_id)) // check if numberic value for userID
+	    {
+		    $result=mysqli_query($con, $query);	
+		    if($result)
+		    {
+			    echo "You have been successfully registered!";
+			    echo "<a href= '../index.html'> Return to login</a>";
+		    }
+		    else
+		    {
+			    echo "(1)There has been an error.";
+			    echo "<a href='../new_user.html'> Try again. </a>";
+			    
+		    }
+	    }
+	    else
+	    {
+		    echo "<h1 class='red-text'>Error!</h1>"; echo "<br>";
+		    echo "(2)User ID must be ALL numeric values!"; echo "<br>";
+		    echo "<a href= '../new_user.html'>Go back</a>";
+	    }
 	}
 	else
 	{
-		echo "<h1 class='red-text'>Error!</h1>"; echo "<br>";
-		echo "User ID must be ALL numeric values!"; echo "<br>";
-		echo "<a href= 'new_user.html'>Go back</a>";
+	    echo "(3)Unable to register... User ID $user_id is already registered.";
+	    echo "<a href='../new_user.html'> Try again. </a>";
 	}
-	
 
 mysqli_close($con);
 ?>
